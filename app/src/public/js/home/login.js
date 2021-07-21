21295202f1257ddd4ccec8780a2b3b54c9987b90
Amenable <duswo0624@naver.com>
2021-07-21 17:11:34 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+    paword = document.querySelector("#psword"),
+    logintBtn = document.querySelector("button");
+
+logintBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        paword: psword.value,
+    };
+    console.log(req);
+
+}
